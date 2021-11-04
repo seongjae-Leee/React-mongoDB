e1a2dce0a806f488dec020616c42eebd55f9005c
Seongjae <robbyl95@hanmail.net>
2021-11-04 09:39:08 +0900

    20211104_add gitignore

--- index.js
+++ index.js
@@ -2,17 +2,37 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+const config = require('./config/key');
 
-mongoose.connect('mongodb+srv://seongjae:1234@sjdb.eipk6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-  useNewUrlParser: true, useUnifiedTopology: true
+// 비밀로 설정한 정보를 가져오는 것(비밀번호 담김)
+mongoose.connect(config.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 }).then(() => console.log('MongoDB Connected~!!'))
   .catch(err => console.log(err));
 
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.post('/register', (req, res) => {
+  //회원가입할 때 필요한 정보들을 client에서 가져오면 그것들을 DB에 넣어준다.
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true
+    });
+  });
+});
+
+
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`http://localhost:${port}에서 만납시다.`);
 });
--- models/User.js
+++ models/User.js
@@ -36,4 +36,4 @@ const userSchema = mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
-module.exports = {};
+module.exports = { User };
