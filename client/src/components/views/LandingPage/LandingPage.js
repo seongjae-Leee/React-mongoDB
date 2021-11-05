baa2e73f9a4aa2e982b914569005c50353ae4488
Seongjae <robbyl95@hanmail.net>
2021-11-05 10:52:13 +0900

    http-proxy-middleware로 CORS policy이슈 해결

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -5,7 +5,7 @@ const LandingPage = () => {
   //▼ 랜딩페이지에 오자마자 실행되는 함수
   useEffect(() => {
     // get 요청을 서버에 보내는 중
-    axios.get('http://localhost:5000/api/hello')
+    axios.get('/api/hello')
       .then(response => console.log('response.data'));
 
   }, []);
